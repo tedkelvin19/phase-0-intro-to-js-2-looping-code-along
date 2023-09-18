78e8ada671cb583f5b4f2a92907f2430723dfdf7
TedKelvin <tedkelvin19@gmail.com>
2023-09-18 09:06:54 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arrayname,eventName){
+    let arrayThankMessage=[]
+    for(let i=0; i<arrayname.length; i++){
+        arrayThankMessage.push(`Thank you, ${arrayname[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return arrayThankMessage
+}
+
+function countDown(num) {
+    while(num>=0){
+        console.log(num)
+        num--
+    }
+};
